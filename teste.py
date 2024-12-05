48e6610513d26a4879f2b634a479641508caecf8
geanmaitha <gean.cruz@maitha.com.br>
2024-12-05 10:51:19 -0300

    fix: corrigindo coisas que o code rabbit pediu/HP2100

--- teste.py
+++ teste.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 
-gean = [12, 12 3, 34, 2, 3.14]
+gean = [12, 12, 3, 34, 2, 3.14]
 
 
 for i in gean:
-    print(f'Segue os números kkk: {i}')/n
+    print(f'Segue os números kkk: {i}')
+
+
+print("Código Finalizado! ")
 
